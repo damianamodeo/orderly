9be495d3bc0d7a56530f63486c38443e312938f9
damianamodeo <damianamodeotheocratic>
2022-07-18 10:18:05 +1000

    add firebase

--- src/component/publisherDetails.js
+++ src/component/publisherDetails.js
@@ -13,12 +13,33 @@ const PublisherDetails = ({
         headerLeft={<HeaderLeft label="Back" actionLeft={viewPublisherList} />}
         title={`${publisher.firstName} ${publisher.middleName} ${publisher.lastName}`}
         headerRight={
-          <HeaderRight label="Edit" actionRight={() => editPublisherDetails(publisher)} />
+          <HeaderRight
+            label="Edit"
+            actionRight={() => editPublisherDetails(publisher)}
+          />
         }
       />
       <div className="publisher-details">
+        {(publisher.mobilePhone.length > 0 ||
+          publisher.homePhone.length > 0) ? 
+          <div className="detail-group">
+            
+          
+            <div className="label-details">mobile</div>
+            <div className="detail">{publisher.mobilePhone}</div> 
 
-        <div>Mobile: {publisher.mobilePhone}</div>
+            <div className="label-details">home</div>
+            <div className="detail">{publisher.homePhone}</div>
+          </div> : null
+        }
+
+        <div>DOB: {publisher.birthDate}</div>
+        <div>Bap: {publisher.baptismDate}</div>
+        <div>DOB: {publisher.birthDate}</div>
+        <div>DOB: {publisher.birthDate}</div>
+        <div>DOB: {publisher.birthDate}</div>
+        <div>DOB: {publisher.birthDate}</div>
+        <div>DOB: {publisher.birthDate}</div>
         <div>DOB: {publisher.birthDate}</div>
       </div>
     </>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore, collection, getDocs
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7UJ5XXULmjlUT28hGlDcedQ8-UPHW2xU",
+  authDomain: "orderly-4bffa.firebaseapp.com",
+  projectId: "orderly-4bffa",
+  storageBucket: "orderly-4bffa.appspot.com",
+  messagingSenderId: "1048111863868",
+  appId: "1:1048111863868:web:43c6f0adcebb10c6504b39",
+};
+
+initializeApp(firebaseConfig); 
+
+const dbfs = getFirestore()
+
+const colRef = collection(dbfs, 'test')
+
+// read firestore database to console
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = []
+    snapshot.docs.forEach((doc) =>{
+      books.push({...doc.data(), id: doc.id})
+    })
+    console.log(books)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
