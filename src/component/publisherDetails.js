adfbcd72cbe6d094d3b5e7e70552a49c7bc643c1
damianamodeo <damianamodeotheocratic>
2022-07-18 15:28:49 +1000

    add phone and address links

--- src/component/publisherDetails.js
+++ src/component/publisherDetails.js
@@ -19,28 +19,46 @@ const PublisherDetails = ({
           />
         }
       />
+
       <div className="publisher-details">
         {(publisher.mobilePhone.length > 0 ||
-          publisher.homePhone.length > 0) ? 
+          publisher.homePhone.length > 0) && (
           <div className="detail-group">
-            
-          
-            <div className="label-details">mobile</div>
-            <div className="detail">{publisher.mobilePhone}</div> 
+            {publisher.mobilePhone.length > 0 && (
+              <>
+                <div className="label-details">mobile</div>
+                <a className="detail" href={`tel://${publisher.mobilePhone}`}>
+                  {publisher.mobilePhone}
+                </a>
+              </>
+            )}
+            {publisher.homePhone.length > 0 && (
+              <>
+                <div className="label-details">home</div>
+                <a className="detail" href={`tel://${publisher.homePhone}`}>
+                  {publisher.homePhone}
+                </a>
+              </>
+            )}
+          </div>
+        )}
 
-            <div className="label-details">home</div>
-            <div className="detail">{publisher.homePhone}</div>
-          </div> : null
-        }
+        {(publisher.house.length > 0 ||
+          publisher.street.length > 0 ||
+          publisher.suburb.length > 0) && (
+          <div className="detail-group">
+            <div className="label-details">address</div>
+            <a
+              className="detail"
+              href={`https://www.google.com/maps/place/${publisher.house}+${publisher.street},+${publisher.suburb}`}
+            >
+              {publisher.house} {publisher.street}, {publisher.suburb}
+            </a>
+          </div>
+        )}
 
         <div>DOB: {publisher.birthDate}</div>
-        <div>Bap: {publisher.baptismDate}</div>
-        <div>DOB: {publisher.birthDate}</div>
-        <div>DOB: {publisher.birthDate}</div>
-        <div>DOB: {publisher.birthDate}</div>
-        <div>DOB: {publisher.birthDate}</div>
-        <div>DOB: {publisher.birthDate}</div>
-        <div>DOB: {publisher.birthDate}</div>
+        <div>Bap: {publisher.baptismDate}</div> 
       </div>
     </>
   );
